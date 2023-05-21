ed88ce4e07bb3f130714c3a7c5fba8b7bca8c21f
jadir99 <mohamad.jadir2018@gmail.com>
2023-05-21 20:40:33 +0100

    i added new charts for numbers of learners in the curent month ,also the last month, i upate the charts bar to line

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Builder;
 
 
 use Illuminate\Http\Request;
+use PHPUnit\Framework\TestCase;
 use Symfony\Contracts\Service\Attribute\Required;
 
 use function PHPUnit\Framework\isEmpty;
@@ -192,6 +193,18 @@ class UserController extends Controller
         return $count;
     }
 
+    // object to array for this and last months     
+    private function to_array($count){
+        $nbr=[];
+        for ($i=0;$i<2;$i++){
+            if (!isset($count[$i]))
+            $nbr[$i]=0;
+            else
+            $nbr[$i]=$count[$i]->count;
+        }
+        return $nbr;
+    }
+
     public function dashboard(){
         // search about the courses of this year
         $courses = course::selectRaw('COUNT(*) as count, MONTH(date_pub) as month')
@@ -218,12 +231,37 @@ class UserController extends Controller
         $reviews_count=$this->search_data($reviews);
 
 
-        // numbers of courses in this month and the last month
+        // numbers of courses in this month and the last month are
+        $nbr_courses = course::selectRaw('COUNT(*) as count, MONTH(date_pub) as month')
+        ->whereYear('date_pub', [date("Y-m",strtotime("-1 month")),date('Y-m')])
+        ->groupBy('month')
+        ->get();
+// CALL THE method to array   
+        $nbr_courses=$this->to_array($nbr_courses);
+
+        // echo $nbr_courses[1];
+
+
+
+        // search about the users of this month and last month
+        $nbr_users = User::selectRaw('COUNT(*) as count, MONTH(created_at) as month')
+        ->whereYear('created_at', [date("Y-m",strtotime("-1 month")),date('Y-m')])
+        ->where('role','user')
+        ->groupBy('month')
+        ->get();
+        $nbr_users=$this->to_array($nbr_users);
+        // echo $nbr_users[1];
+
         
 
 
 
-        return view('users.dashboeard_admin',['courses'=>$cours_count,'users_count'=>$users_count,'reviews_count'=>$reviews_count]);
+
+
+
+
+
+        return view('users.dashboeard_admin',['courses'=>$cours_count,'users_count'=>$users_count,'reviews_count'=>$reviews_count,'nbr_courses'=>$nbr_courses,'nbr_users'=>$nbr_users]);
 
     }
 }
--- resources/views/users/dashboeard_admin.blade.php
+++ resources/views/users/dashboeard_admin.blade.php
@@ -38,28 +38,18 @@ E-Learning dashboard
 </div>
 
 
-<div class="card m-3">
+<div class="card mt-3">
     <div class="card-body px-xxl-0 pt-4">
       <div class="row g-0">
         <div class="col-xxl-3 col-md-6 px-3 text-center border-end-md border-bottom border-bottom-xxl-0 pb-3 p-xxl-0 ps-md-0">
           <div class="icon-circle icon-circle-primary"><span class="fs-2 fas fa-user-graduate text-primary"></span></div>
-          <h4 class="mb-1 font-sans-serif"><span class="text-700 mx-2" data-countup='{"endValue":"10"}'>0</span><span class="fw-normal text-600">New Learners</span></h4>
-          <p class="fs--1 fw-semi-bold mb-0">4203 <span class="text-600 fw-normal">last month</span></p>
+          <h4 class="mb-1 font-sans-serif"><span class="text-700 mx-2" data-countup='{"endValue":"{{$nbr_users[0]}}"}'>0</span><span class="fw-normal text-600">New Learners</span></h4>
+          <p class="fs--1 fw-semi-bold mb-0">{{$nbr_users[1]}} <span class="text-600 fw-normal">last month</span></p>
         </div>
         <div class="col-xxl-3 col-md-6 px-3 text-center border-end-xxl border-bottom border-bottom-xxl-0 pb-3 pt-4 pt-md-0 pe-md-0 p-xxl-0">
-          <div class="icon-circle icon-circle-info"><span class="fs-2 fas fa-chalkboard-teacher text-info"></span></div>
-          <h4 class="mb-1 font-sans-serif"><span class="text-700 mx-2" data-countup='{"endValue":"324"}'>0</span><span class="fw-normal text-600">New Trainers</span></h4>
-          <p class="fs--1 fw-semi-bold mb-0">301 <span class="text-600 fw-normal">last month</span></p>
-        </div>
-        <div class="col-xxl-3 col-md-6 px-3 text-center border-end-md border-bottom border-bottom-md-0 pb-3 pt-4 p-xxl-0 pb-md-0 ps-md-0">
-          <div class="icon-circle icon-circle-success"><span class="fs-2 fas fa-book-open text-success"></span></div>
-          <h4 class="mb-1 font-sans-serif"><span class="text-700 mx-2" data-countup='{"endValue":"3712"}'>0</span><span class="fw-normal text-600">New Courses</span></h4>
-          <p class="fs--1 fw-semi-bold mb-0">2779 <span class="text-600 fw-normal">last month</span></p>
-        </div>
-        <div class="col-xxl-3 col-md-6 px-3 text-center pt-4 p-xxl-0 pb-0 pe-md-0">
-          <div class="icon-circle icon-circle-warning"><span class="fs-2 fas fa-dollar-sign text-warning"></span></div>
-          <h4 class="mb-1 font-sans-serif"><span class="text-700 mx-2" data-countup='{"endValue":"1054"}'>0</span><span class="fw-normal text-600">Refunds</span></h4>
-          <p class="fs--1 fw-semi-bold mb-0">1201 <span class="text-600 fw-normal">last month</span></p>
+            <div class="icon-circle icon-circle-success"><span class="fs-2 fas fa-book-open text-success"></span></div>
+            <h4 class="mb-1 font-sans-serif"><span class="text-700 mx-2" data-countup='{"endValue":"{{$nbr_courses[1]}}"}'>0</span><span class="fw-normal text-600">New Courses</span></h4>
+            <p class="fs--1 fw-semi-bold mb-0">{{$nbr_courses[0]}} <span class="text-600 fw-normal">last month</span></p>
         </div>
       </div>
     </div>
@@ -67,55 +57,53 @@ E-Learning dashboard
 
   {{-- charts for users courses reviews --}}
 <div>
-        <canvas id="myChart" class="card col m-5"></canvas>
-    </div>
-  
-  
+    <canvas id="myChart" class="card col mt-3 p-7"></canvas>
+</div>
   
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   
-  <script>
+<script>
     // Retrieve the data from the server-side variable
   
     // Create the chart
     const ctx = document.getElementById('myChart').getContext('2d');
     new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
             labels: ['January', 'February', 'March', 'April', 'May', 'June','July','August','September','October','November','December'],
             datasets: [
                 {
                 label: 'number of courses',
-                data: [{{$courses[1]}}, {{$courses[2]}},{{$courses[3]}}+0, {{$courses[4]}}, {{$courses[5]}}, {{$courses[6]}},{{$courses[7]}}, {{$courses[8]}}, {{$courses[9]}}, {{$courses[10]}}, {{$courses[11]}}, {{$courses[12]}}],
-                backgroundColor: [
-                    '#0B2545',
-                ],
+                data: [{{$courses[1]}}, {{$courses[2]}},{{$courses[3]}}, {{$courses[4]}}, {{$courses[5]}}, {{$courses[6]}},{{$courses[7]}}, {{$courses[8]}}, {{$courses[9]}}, {{$courses[10]}}, {{$courses[11]}}, {{$courses[12]}}],
+                
                 borderColor: [
-                  'black',
+                  'red',
                 ],
-                borderWidth: 1
-            },
-            {
-                label: 'number of users',
-                data: [{{$users_count[1]}}, {{$users_count[2]}},{{$users_count[3]}}+0, {{$users_count[4]}}, {{$users_count[5]}}, {{$users_count[6]}},{{$users_count[7]}}, {{$users_count[8]}}, {{$users_count[9]}}, {{$users_count[10]}}, {{$users_count[11]}}, {{$users_count[12]}}],
-                backgroundColor: [
-                    '#134074',
-                ],
-                borderColor: [
-                  'black',
-                ],
-                borderWidth: 1
+                pointRadius: 10,
+                pointBackgroundColor: 'red',
+                borderWidth: 4
             },
             {
                 label: 'number of reviews',
-                data: [{{$reviews_count[1]}}, {{$reviews_count[2]}},{{$reviews_count[3]}}+0, {{$reviews_count[4]}}, {{$reviews_count[5]}}, {{$reviews_count[6]}},{{$reviews_count[7]}}, {{$reviews_count[8]}}, {{$reviews_count[9]}}, {{$reviews_count[10]}}, {{$reviews_count[11]}}, {{$reviews_count[12]}}],
-                backgroundColor: [
-                    '#8DA9C4',
-                ],
+                data: [{{$reviews_count[1]}}, {{$reviews_count[2]}},{{$reviews_count[3]}}, {{$reviews_count[4]}}, {{$reviews_count[5]}}, {{$reviews_count[6]}},{{$reviews_count[7]}}, {{$reviews_count[8]}}, {{$reviews_count[9]}}, {{$reviews_count[10]}}, {{$reviews_count[11]}}, {{$reviews_count[12]}}],
+                
                 borderColor: [
-                  'black',
+                  'green',
                 ],
-                borderWidth: 1
+                pointRadius: 10,
+                pointBackgroundColor: 'green',
+                borderWidth: 4
+            },
+            {
+                label: 'number of users',
+                data: [{{$users_count[1]}}, {{$users_count[2]}},{{$users_count[3]}}, {{$users_count[4]}}, {{$users_count[5]}}, {{$users_count[6]}},{{$users_count[7]}}, {{$users_count[8]}}, {{$users_count[9]}}, {{$users_count[10]}}, {{$users_count[11]}}, {{$users_count[12]}}],
+                
+                borderColor: [
+                  'blue',
+                ],
+                pointRadius: 10,
+                pointBackgroundColor: 'blue',
+                borderWidth: 4
             }
         ]
         },
@@ -124,8 +112,20 @@ E-Learning dashboard
                 y: {
                     beginAtZero: true
                 }
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            size: 25, // Adjust the size of the labels
+                            family: 'Arial' ,// Adjust the font family of the labels
+
+                        }
+                    }
+                }
             }
         }
     });
-  </script>   
+
+  </script>    
 @endsection
